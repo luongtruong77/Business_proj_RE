9d8eb4cece45ad840e53511f4504bf239c705782
Luong Q Truong <tqluong77@gmail.com>
2021-04-27 13:25:43 -0700

    create tableau workbook

--- Recommender_System/Evaluator.py
+++ Recommender_System/Evaluator.py
@@ -50,7 +50,7 @@ class Evaluator:
             print("           for a given user. Higher means more diverse.")
             print("Novelty:   Average popularity rank of recommended items. Higher means more novel.")
 
-    def SampleTopNRecs(self, ml, testSubject=85, k=10):
+    def SampleTopNRecs(self, ml, testSubject=793, k=10):
 
         for algo in self.algorithms:
             print("\nUsing recommender ", algo.GetName())
